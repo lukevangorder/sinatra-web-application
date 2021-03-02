3c894bc0d24bfd45bd6b2c35a3f5278711334cde
lukevangorder <lukevangorder@gmail.com>
2021-03-02 16:34:57 -0500

    Added activerecord and sqlite3 gems

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < Sinatra::Base
 
+    configure do
+        set :views, 'app/views'
+        set :public_folder, 'public'
+    end
+
+    get '/' do
+        erb :index
+    end
+
 end
